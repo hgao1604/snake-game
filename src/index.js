168f7e5623f9a30adb3b1d610eb3f758748a8f62
highcold <hgao1604@usc.edu>
2023-06-08 22:40:26 -0700

    fix the bug related to setInterval by using useRef

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import APP2 from "./APP2";
+import App3 from "./App3";
 
-ReactDOM.render(<APP2 />, document.getElementById("root"));
+ReactDOM.render(<App3 />, document.getElementById("root"));
